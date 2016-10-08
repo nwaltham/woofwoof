aab1db609e9b6a5d5d628f9d025a75361ba4b78d
Nicholas Waltham <nw221@log1.net>
2016-10-08 12:47:34 +0200

    fixed xo linter errors

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-/*jshint node:true */
-/*jshint esversion:6 */
+/* jshint node:true */
+/* jshint esversion:6 */
 'use strict';
 
-const Promise = require("bluebird");
+const os = require('os');
 const path = require('path');
 const camelcaseKeys = require('camelcase-keys');
 const decamelizeKeys = require('decamelize-keys');
@@ -11,144 +11,140 @@ const redent = require('redent');
 const readPkgUp = require('read-pkg-up');
 const loudRejection = require('loud-rejection');
 const normalizePackageData = require('normalize-package-data');
-const fs = Promise.promisifyAll(require('fs'));
-const os = require("os");
-const nconf = require("nconf");
-const mkdirp = Promise.promisifyAll(require("mkdirp"));
+const nconf = require('nconf');
+const mkdirp = Promise.promisifyAll(require('mkdirp'));
 
 // prevent caching of this module so module.parent is always accurate
 delete require.cache[__filename];
 const parentDir = path.dirname(module.parent.filename);
 
 module.exports = (opts, minimistOpts) => {
-    loudRejection();
+	loudRejection();
 
-    if (Array.isArray(opts) || typeof opts === 'string') {
-        opts = {
-            help: opts
-        };
-    }
+	if (Array.isArray(opts) || typeof opts === 'string') {
+		opts = {
+			help: opts
+		};
+	}
 
-    opts = Object.assign({
-        pkg: readPkgUp.sync({
-            cwd: parentDir,
-            normalize: false
-        }).pkg,
-        argv: process.argv.slice(2),
-        inferType: false
-    }, opts);
+	opts = Object.assign({
+		pkg: readPkgUp.sync({
+			cwd: parentDir,
+			normalize: false
+		}).pkg,
+		argv: process.argv.slice(2),
+		inferType: false
+	}, opts);
 
-    minimistOpts = Object.assign({
-        string: ['_']
-    }, minimistOpts);
+	minimistOpts = Object.assign({
+		string: ['_']
+	}, minimistOpts);
 
-    minimistOpts.default = decamelizeKeys(minimistOpts.default || {}, '-');
+	minimistOpts.default = decamelizeKeys(minimistOpts.default || {}, '-');
 
-    const index = minimistOpts.string.indexOf('_');
+	const index = minimistOpts.string.indexOf('_');
 
-    if (opts.inferType === false && index === -1) {
-        minimistOpts.string.push('_');
-    } else if (opts.inferType === true && index !== -1) {
-        minimistOpts.string.splice(index, 1);
-    }
+	if (opts.inferType === false && index === -1) {
+		minimistOpts.string.push('_');
+	} else if (opts.inferType === true && index !== -1) {
+		minimistOpts.string.splice(index, 1);
+	}
 
-    const pkg = opts.pkg;
-  /*instrumentation*/
-  /*
+	const pkg = opts.pkg;
+	/* instrumentation*/
+	/*
     console.log('"opts"');
     console.log(JSON.stringify(opts, null, 4));
     console.log('"minimistOpts"');
     console.log(JSON.stringify(minimistOpts, null, 4));
     //const argv = minimist(opts.argv, minimistOpts);
 */
-  
-    const settingsdir = "." + opts.pkg.name;
 
-  /*instrumentation*/
-    //console.log(os.homedir());
-    var configpath = path.join(os.homedir(), settingsdir);
-    var settingsfile = path.join(configpath, "settings.json");
-    mkdirp.sync(configpath);
-    
-    // TODO: add alias conversion
+	const settingsdir = '.' + opts.pkg.name;
 
-    const argv = nconf.argv()
-        .env()
-        .file({
-            file: settingsfile
-        });
+	/* instrumentation*/
+	// console.log(os.homedir());
+	const configpath = path.join(os.homedir(), settingsdir);
+	const settingsfile = path.join(configpath, 'settings.json');
+	mkdirp.sync(configpath);
 
-//instrumentation
-  //console.log(JSON.stringify(nconf, null, 4));
+	// TODO: add alias conversion
 
-    let help = redent(trimNewlines((opts.help || '').replace(/\t+\n*$/, '')), 2);
+	const argv = nconf.argv()
+		.env()
+		.file({
+			file: settingsfile
+		});
 
-    normalizePackageData(pkg);
+	// instrumentation
+	// console.log(JSON.stringify(nconf, null, 4));
 
-    process.title = pkg.bin ? Object.keys(pkg.bin)[0] : pkg.name;
+	let help = redent(trimNewlines((opts.help || '').replace(/\t+\n*$/, '')), 2);
 
-    let description = opts.description;
-    if (!description && description !== false) {
-        description = pkg.description;
-    }
+	normalizePackageData(pkg);
 
-    help = (description ? `\n  ${description}\n` : '') + (help ? `\n${help}\n` : '\n');
+	process.title = pkg.bin ? Object.keys(pkg.bin)[0] : pkg.name;
 
-    const showHelp = code => {
-        console.log(help);
-        process.exit(typeof code === 'number' ? code : 2);
-    };
+	let description = opts.description;
+	if (!description && description !== false) {
+		description = pkg.description;
+	}
 
-    const saveSettings = code => {
-        console.log("Saving Settings");
-        var newargs = nconf.stores.argv.store;
-        //just save the newly specified arguments
-        delete newargs._;
-        delete newargs['save-settings'];
-        delete newargs['saveSettings'];
-        delete newargs['$0'];
-        //instrumentation
-        //console.log(Object.keys(newargs));
-        let keys = Object.keys(newargs);
-        let keysl = keys.length;
-        for (var i = 0; i < keysl; i++) {
-            var k = keys[i];
-            nconf.set(k, nconf.get(k));
-        };
-        nconf.save(settingsfile);
+	help = (description ? `\n  ${description}\n` : '') + (help ? `\n${help}\n` : '\n');
 
-        process.exit(typeof code === 'number' ? code : 2);
-    };
+	const showHelp = code => {
+		console.log(help);
+		process.exit(typeof code === 'number' ? code : 2);
+	};
 
+	const saveSettings = code => {
+		console.log('Saving Settings');
+		const newargs = nconf.stores.argv.store;
+		// just save the newly specified arguments
+		delete newargs._;
+		delete newargs['save-settings'];
+		delete newargs.saveSettings;
+		delete newargs.$0;
+		// instrumentation
+		// console.log(Object.keys(newargs));
+		const keys = Object.keys(newargs);
+		const keysl = keys.length;
+		for (let i = 0; i < keysl; i++) {
+			const k = keys[i];
+			nconf.set(k, nconf.get(k));
+		}
+		nconf.save(settingsfile);
 
-    if (argv.get("version") && opts.version !== false) {
-        console.log(typeof opts.version === 'string' ? opts.version : pkg.version);
-        process.exit();
-    }
+		process.exit(typeof code === 'number' ? code : 2);
+	};
 
-    if (argv.get("help") && opts.help !== false) {
-        showHelp(0);
-    }
+	if (argv.get('version') && opts.version !== false) {
+		console.log(typeof opts.version === 'string' ? opts.version : pkg.version);
+		process.exit();
+	}
 
-    if (argv.get("save-settings") && opts["save-settings"] !== false) {
-        saveSettings(0);
-    }
+	if (argv.get('help') && opts.help !== false) {
+		showHelp(0);
+	}
 
+	if (argv.get('save-settings') && opts['save-settings'] !== false) {
+		saveSettings(0);
+	}
 
-    const input = argv.get('_');
-    //delete argv._;
-    var arfv = argv.get();
-    delete arfv._;
+	const input = argv.get('_');
+	// delete argv._;
+	const arfv = argv.get();
+	delete arfv._;
 
-    const flags = camelcaseKeys(arfv, {
-        exclude: ['--', /^\w$/]
-    });
+	const flags = camelcaseKeys(arfv, {
+		exclude: ['--', /^\w$/]
+	});
 
-    return {
-        input,
-        flags,
-        pkg,
-        help,
-        showHelp
-    };
+	return {
+		input,
+		flags,
+		pkg,
+		help,
+		showHelp
+	};
 };
